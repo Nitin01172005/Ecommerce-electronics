a7aa2307d239eb85076645323c3c5eda0ad62a9a
NITIN KIROLA <nitinkirola@NITINs-MacBook-Air.local>
2025-05-28 23:46:33 +0530

    Add changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
+import React from "react";
 import Navbar from "./Components/Navbar.jsx";
 
-import Hero from './Components/Hero.jsx'
+import Hero from "./Components/Hero.jsx";
 import Category from "./Components/Category.jsx";
 import Mid from "./Components/Mid.jsx";
 import Products from "./Components/Products.jsx";
 import Banner from "./Components/Banner.jsx";
 import News from "./Components/News.jsx";
 import Bottom from "./Components/Bottom.jsx";
-import Footer from './Components/Footer.jsx';
+import Footer from "./Components/Footer.jsx";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
 const App = () => {
-
   React.useEffect(() => {
     AOS.init({
       offset: 100,
@@ -22,22 +21,21 @@ const App = () => {
       delay: 100,
     });
     AOS.refresh();
-  },[])
-
+  }, []);
 
   return (
-    <div className=' dark:bg-gray-900'>
-    <Navbar />
-    <Hero />
-    <Category />
-    <Mid />
-    <Products />
-    <Banner />
-    <News />
-    <Bottom />
-    <Footer />
+    <div className=" dark:bg-gray-900">
+      <Navbar />
+      <Hero />
+      <Category />
+      <Mid />
+      <Products />
+      <Banner />
+      <News />
+      <Bottom />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
