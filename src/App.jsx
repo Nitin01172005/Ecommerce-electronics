f4ff73361a42beaf15aa69669e3940ca68b62552
NITIN KIROLA <nitinkirola@NITINs-MacBook-Air.local>
2025-05-28 23:41:19 +0530

    Add changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Navbar from "./Components/Navbar.jsx";
 
-import Hero from './Components/Hero'
-import Category from './Components/Category'
-import Mid from './Components/Mid'
-import Products from './Components/Products'
-import Banner from './Components/Banner'
-import News from './Components/News'
-import Bottom from './Components/Bottom'
-import Footer from './Components/Footer';
+import Hero from './Components/Hero.jsx'
+import Category from "./Components/Category.jsx";
+import Mid from "./Components/Mid.jsx";
+import Products from "./Components/Products.jsx";
+import Banner from "./Components/Banner.jsx";
+import News from "./Components/News.jsx";
+import Bottom from "./Components/Bottom.jsx";
+import Footer from './Components/Footer.jsx';
 import AOS from "aos";
 import "aos/dist/aos.css";
 
